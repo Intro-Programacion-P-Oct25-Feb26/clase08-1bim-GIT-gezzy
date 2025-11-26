617ed4e40fadd3740d54f4d884f237fb22dbab06
GIT-gezzy <jdavichomaldonado@gmail.com>
2025-11-26 11:54:22 -0500

    solucion problema 2

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
@@ -31,11 +31,14 @@ public class Ejemplo02 {
             cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
             entrada.nextLine(); // limpieza de buffer
 
-            System.out.println("Ingrese (s) si desea salir del ciclo ");
+            System.out.println("Ingrese (s, si, y o yes) si desea salir del "
+                    + "ciclo ");
             salida = entrada.nextLine();
-
             
-            
+            if ((salida.equals("s"))||(salida.equals("si"))||
+                    (salida.equals("y"))||(salida.equals("yes"))) { // f
+                bandera = false;
+            }
 
         } while (bandera); // (bandera==true)
 
